72b8ecde0f8f4ba37a63616bf274bce0bc381675
Vrushti-coding <patelvrushti228@gmail.com>
2025-05-13 11:39:10 +0530

    Add File

---
+++ Factorial.py
@@ -0,0 +1,8 @@
+# Date:13/5/2025
+# Author:Vrushti Patel
+# program:find factorial number
+fact=1
+no=input("Enter a number")
+for i in range(1,int(no)+1):
+    fact*=i
+print(fact)
---
+++ Simple_Interest.py
@@ -0,0 +1,7 @@
+# Date:13/5/2025
+# Author:Vrushti Patel
+# program:Find simple iterest
+p=5000
+t=5
+r=2.5
+print("simple interest:",p*r*t/100)
